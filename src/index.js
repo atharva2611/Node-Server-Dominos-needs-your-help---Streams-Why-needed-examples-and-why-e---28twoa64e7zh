0b705c087abf27a2c55d916a950a9c2f60fb0cb3
atharva2611 <83395780+atharva2611@users.noreply.github.com>
2023-02-25 14:49:32 +0530

    updated-index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+const { NOTFOUND } = require("dns");
 var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    if (req.url == '/welcome') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("Welcome to Dominos!");
+}else if(req.url == '/contact'){
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ 
+        phone: '18602100000', 
+        email: 'guestcaredominos@jublfood.com' 
+      }));  
+    res.end(); 
+}else{
+    // res.WriteHead(404);
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end("404 Not found");
+
 }
+}
+
+httpServer.listen(8081);
+console.log('Node.js web server at port 5000 is running..')
 
 module.exports = httpServer;
